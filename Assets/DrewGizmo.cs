31b22acb4a45ef04de1c677c457828e4a2483207
Eadmond <eadmond@hotmail.com>
2021-03-12 23:18:03 -0800

    Added an enemy placeholder that can chase player if in range

---
+++ Assets/DrewGizmo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrewGizmo : MonoBehaviour
+{
+    [SerializeField] float gizmoRange = 10f;
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, gizmoRange);
+    }
+}
---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 10f;
+
+    NavMeshAgent agent;
+    float distanceToTarget = Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void TryChaseTarget()
+    {
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarget <= chaseRange)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TryChaseTarget();
+    }
+}
