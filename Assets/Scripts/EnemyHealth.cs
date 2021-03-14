644ce1f33a16424e9d65704a1db21cb28560a899
Eadmond <eadmond@hotmail.com>
2021-03-13 16:35:10 -0800

    Added muzzle flash

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+
+    public void OnHit(int damange)
+    {
+        health -= damange;
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MyWeapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyWeapon : MonoBehaviour
+{
+    [SerializeField] Camera myCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] int damage = 10;
+    [SerializeField] ParticleSystem muzzleFlash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }        
+    }
+
+    void Shoot()
+    {
+        CheckHit();
+        PlayMuzzleFlash();
+    }
+
+    void CheckHit()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(myCamera.transform.position, myCamera.transform.forward, out hit, range))
+        {
+            Debug.Log("I hit " + hit.transform.name);
+            EnemyHealth health = hit.transform.GetComponent<EnemyHealth>();
+            if (health)
+                health.OnHit(damage);
+        }
+    }
+
+    void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+}
