d20b5c045fe051b7b864709e7bbd44ea9c35da34
Eadmond <eadmond@hotmail.com>
2022-10-12 16:02:42 -0700

    Enemy didn't get removed after death

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -10,11 +10,6 @@ public class EnemyHealth : MonoBehaviour
 
     bool isDead = false;
 
-    private void OnEnable()
-    {
-        GetComponent<Animator>().SetBool("Die", false);
-    }
-
     public void OnHit(int damage)
     {
         if (isDead) return;
@@ -25,7 +20,7 @@ public class EnemyHealth : MonoBehaviour
 
         if(health <= 0)
         {
-            GetComponent<Animator>().SetBool("Die", true);
+            GetComponentInChildren<Animator>().SetTrigger("Die");
             GetComponent<AudioSource>().Stop();
             myCollider.enabled = false;
             isDead = true;
